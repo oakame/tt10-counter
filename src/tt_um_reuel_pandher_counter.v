77038b72eb16ee6bc6526e749daeaa50060af930
oakame <reuellpandher@gmail.com>
2026-02-02 18:41:11 -0600

    Update tt_um_reuel_pandher_counter.v

--- src/tt_um_reuel_pandher_counter.v
+++ src/tt_um_reuel_pandher_counter.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_islam_ihfaz_counter (
+module tt_um_reuel_pandher_counter (
 input wire [7:0] ui_in, // Dedicated inputs
 output wire [7:0] uo_out, // Dedicated outputs
 input wire [7:0] uio_in, // IOs: Input path
